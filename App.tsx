980efe8a64d2c4be3a74ef68b0067c490cc268f1
Caio Mori <caiomori22@gmail.com>
2022-07-18 17:42:54 -0300

    fix(yarnlock): fix changes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import SignInPage from "./src/screens/signIn";
-import { NativeBaseProvider } from "native-base";
+import React from 'react'
+import SignInPage from './src/screens/signIn'
+import {NativeBaseProvider} from 'native-base'
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <SignInPage />
     </NativeBaseProvider>
-  );
+  )
 }
