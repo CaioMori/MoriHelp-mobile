4e4be6b013cff90e215413c24c592a381da23674
Caio Mori <caiomori22@gmail.com>
2022-07-18 18:32:12 -0300

    feat(svg): add svg support

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {VStack, Heading} from 'native-base'
+import Logo from '../../assets/logo_primary.svg'
 
 const SignInPage = () => {
   return (
     <VStack flex={1} bg={'gray.600'} alignItems='center' px={8} pt={24}>
+      <Logo />
       <Heading color={'gray.100'} fontSize={'xl'} mt={20} mb={26}>
         SignInPage
       </Heading>
