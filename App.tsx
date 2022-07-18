ac254eabde3992a450c667c6091fdb98412dad3c
Caio Mori <caiomori22@gmail.com>
2022-07-18 17:26:56 -0300

    feat(configuration): added to the project, custom configuration files
    prettier, estling, husky...

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import SignInPage from "./src/screens/signIn";
+import React from 'react'
+import SignInPage from './src/screens/signIn'
 
 export default function App() {
-  return <SignInPage />;
+  return <SignInPage />
 }
