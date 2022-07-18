eb1d71310ec410838b20f5bbfdd21635f11b7897
Caio Mori <caiomori22@gmail.com>
2022-07-18 17:14:39 -0300

    feat(native-base): add native-base to the project

--- App.tsx
+++ App.tsx
@@ -2,7 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SignInPage from "./src/screens/signIn";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
-  return <SignInPage />;
+  return (
+    <NativeBaseProvider>
+      <SignInPage />
+    </NativeBaseProvider>
+  );
 }
